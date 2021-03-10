62696e1b968866d5185e5c34720e2a3f4a6e985d
sabaly <tmssam47@gmail.com>
2021-03-10 00:21:06 +0000

    Random users generator

--- src/sections/RandomUsers.js
+++ src/sections/RandomUsers.js
@@ -14,91 +14,92 @@ export default function RandomUsers() {
             <Text textAlign="center" m={5}>Play with my random user generator</Text>
             <Get url="https://randomuser.me/api/">
                 {(error, response, isLoading, makeRequest, axios) => {
-                if(error) {
-                    return (<Box>Something bad happened: {error.message} <button onClick={() => makeRequest({ params: { reload: true } })}>Retry</button></Box>)
-                }
-                else if(isLoading) {
-                    return (
-                        <Box>
-                            <Button
-                                isLoading
-                                colorScheme="green.800"
-                            >
-                            </Button>
-                        </Box>
-                    )
-                }
-                else if(response !== null) {
-                    console.log(response.data.results)
-                    return (
-                        <Box>
-                            <Button 
-                                onClick={() => makeRequest({ params: { refresh: true } })}
-                                colorScheme="green.800" 
-                                variant="outline"
-                            >
-                                    Generate
-                            </Button>
+                    if(error) {
+                        return (<Box>Something bad happened: {error.message} <button onClick={() => makeRequest({ params: { reload: true } })}>Retry</button></Box>)
+                    }
+                    else if(isLoading) {
+                        return (
+                            <Box>
+                                <Button
+                                    isLoading
+                                    colorScheme="white"
+                                    bg="green.800"
+                                >
+                                </Button>
+                            </Box>
+                        )
+                    }
+                    else if(response !== null) {
+                        console.log(response.data.results)
+                        return (
+                            <Box>
+                                <Button 
+                                    onClick={() => makeRequest({ params: { refresh: true } })}
+                                    colorScheme="green.800" 
+                                    variant="outline"
+                                >
+                                        Generate
+                                </Button>
 
-                            {response.data.results.map(result =>
-                                <>
-                                    <Center color="gray.800" key={result.id} flexDirection="column">
-                                        <Image src={result.picture.large} borderRadius="50%" alt={result.name.first + " " +  result.name.last}/>
-                                    </Center> 
+                                {response.data.results.map(result =>
+                                    <>
+                                        <Center color="gray.800" key={result.id} flexDirection="column">
+                                            <Image src={result.picture.large} borderRadius="50%" alt={result.name.first + " " +  result.name.last}/>
+                                        </Center> 
 
-                                    <Tabs variant="enclosed" colorScheme="teal.800" mt={5}>
-                                        <TabList bg="green.800" color="white" fontSize={25}
-                                            borderBottomRadius={30}
-                                        >
-                                            <Tab _selected={{color: "green.800", bg: "white"}}><BiUserCircle /></Tab>
-                                            <Tab _selected={{color: "green.800", bg: "white"}}><BiEnvelope /></Tab>
-                                            <Tab _selected={{color: "green.800", bg: "white"}}><BiCalendar /></Tab>
-                                            <Tab _selected={{color: "green.800", bg: "white"}}><IoLocationOutline /></Tab>
-                                            <Tab _selected={{color: "green.800", bg: "white"}}><BiPhone /></Tab>
-                                            <Tab _selected={{color: "green.800", bg: "white"}}><BiLock /></Tab>
-                                        </TabList>
-                                        <TabPanels>
-                                            <TabPanel>
-                                                <Text textAlign="center">
-                                                    I am {result.name.title + " " + result.name.first + " " +  result.name.last}
-                                                </Text>
-                                            </TabPanel>
-                                            <TabPanel>
-                                               <Text textAlign="center">My email is {result.email}</Text>
-                                            </TabPanel>
-                                            <TabPanel>
-                                                <Text textAlign="center">
-                                                    My birthday is 
-                                                    {
-                                                        formatDate(result.dob.date)
-                                                    }
-                                                </Text>
-                                            </TabPanel>
-                                            <TabPanel>
-                                                <Text textAlign="center">
-                                                    My address is 
-                                                    {" " + result.location.street.number + " " + result.location.street.name + ";\n"}
-                                                    { result.location.city + "/" + result.location.country}
-                                                </Text>
-                                            </TabPanel>
-                                            <TabPanel>
-                                                <Text textAlign="center">
-                                                    My Phone is {" " + result.phone}
-                                                </Text>
-                                            </TabPanel>
-                                            <TabPanel>
-                                                <Text textAlign="center">
-                                                    My password is { " " + result.login.password}
-                                                </Text>
-                                            </TabPanel>
-                                        </TabPanels>
-                                    </Tabs>
-                                </>
-                            )}
-                        </Box>
-                    )
-                }
-                return (<Box>Default message before request is made.</Box>)
+                                        <Tabs variant="enclosed" colorScheme="teal.800" mt={5}>
+                                            <TabList bg="green.800" color="white" fontSize={25}
+                                                borderBottomRadius={30}
+                                            >
+                                                <Tab _selected={{color: "green.800", bg: "white"}}><BiUserCircle /></Tab>
+                                                <Tab _selected={{color: "green.800", bg: "white"}}><BiEnvelope /></Tab>
+                                                <Tab _selected={{color: "green.800", bg: "white"}}><BiCalendar /></Tab>
+                                                <Tab _selected={{color: "green.800", bg: "white"}}><IoLocationOutline /></Tab>
+                                                <Tab _selected={{color: "green.800", bg: "white"}}><BiPhone /></Tab>
+                                                <Tab _selected={{color: "green.800", bg: "white"}}><BiLock /></Tab>
+                                            </TabList>
+                                            <TabPanels>
+                                                <TabPanel>
+                                                    <Text textAlign="center">
+                                                        I am {result.name.title + " " + result.name.first + " " +  result.name.last}
+                                                    </Text>
+                                                </TabPanel>
+                                                <TabPanel>
+                                                <Text textAlign="center">My email is {result.email}</Text>
+                                                </TabPanel>
+                                                <TabPanel>
+                                                    <Text textAlign="center">
+                                                        My birthday is 
+                                                        {
+                                                            formatDate(result.dob.date)
+                                                        }
+                                                    </Text>
+                                                </TabPanel>
+                                                <TabPanel>
+                                                    <Text textAlign="center">
+                                                        My address is 
+                                                        {" " + result.location.street.number + " " + result.location.street.name + ";\n"}
+                                                        { result.location.city + "/" + result.location.country}
+                                                    </Text>
+                                                </TabPanel>
+                                                <TabPanel>
+                                                    <Text textAlign="center">
+                                                        My Phone is {" " + result.phone}
+                                                    </Text>
+                                                </TabPanel>
+                                                <TabPanel>
+                                                    <Text textAlign="center">
+                                                        My password is { " " + result.login.password}
+                                                    </Text>
+                                                </TabPanel>
+                                            </TabPanels>
+                                        </Tabs>
+                                    </>
+                                )}
+                            </Box>
+                        )
+                    }
+                    return (<Box>Default message before request is made.</Box>)
                 }}
             </Get>
 
